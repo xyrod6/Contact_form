f9147720dd8a007daa72facb19f8adaffe890301
xyrod6 <69692905+xyrod6@users.noreply.github.com>
2022-07-15 15:31:46 +0200

    Save to database

--- project/src/Controller/PersonController.php
+++ project/src/Controller/PersonController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Person;
 use App\Form\PersonType;
@@ -12,18 +13,20 @@ use App\Form\PersonType;
 class PersonController extends AbstractController
 {
     #[Route('/person', name: 'app_person')]
-    public function new(Request $request): Response
+    public function saveComplaint(ManagerRegistry $doctrine, Request $request): Response
     {
+		$manager = $doctrine->getManager();
+		
 		$person = new Person();
 		
 		$form = $this->createForm(PersonType::class, $person);
 		$form->handleRequest($request);
 		if($form->isSubmitted() && $form->isValid()){
-		  $person = $form->getData();
-		  //$manager = $this->getDoctrine()->getManager();      
-          //$manager -> persist($person);
-          //$manager -> flush();
-		  return $this->redirectToRoute('task_success');
+		  $person = $form->getData();  
+          $manager -> persist($person);
+          $manager -> flush();
+		  return $this->render('person/success.html.twig');
+		  
 		}
         return $this->render('person/index.html.twig', [
             'form' => $form->createView(),
