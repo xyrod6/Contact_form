6bf7a7b10dcf229dee99e78d64d920ac67ec4618
xyrod6 <69692905+xyrod6@users.noreply.github.com>
2022-07-15 12:43:21 +0200

    Controller added

---
+++ project/src/Controller/PersonController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Person;
+
+class PersonController extends AbstractController
+{
+    #[Route('/person', name: 'app_person')]
+    public function index(): Response
+    {
+		$person = new Person();
+		
+		$form = $this->createForm()
+        return $this->render('person/index.html.twig', [
+            'form' => 'PersonController',
+        ]);
+    }
+}
